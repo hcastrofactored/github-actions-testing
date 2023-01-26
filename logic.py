9eda1d46c2802ac6b3a39dc94e822e2831983a82
HasslerC <hassler@factored.ai>
2023-01-26 12:33:38 -0500

    adding lintern

--- logic.py
+++ logic.py
@@ -3,8 +3,6 @@ def to_upper(input_str):
     return input_str.upper()
 
 
-def to_lower(input_str): 
+def to_lower(input_str):
 
     return input_str.lower()
-
-    
--- test_logic.py
+++ test_logic.py
@@ -1,9 +1,9 @@
-import pytest
-
 from logic import to_lower, to_upper
+
 
 def test_to_lower():
     assert to_lower("HoLA") == "hola"
 
-def test_to_upper(): 
+
+def test_to_upper():
     assert to_upper("HoLa") == "HOLA"
